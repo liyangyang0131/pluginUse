49f30366e6625cf9aaf7ce2336832b369d8987d1
liyangyang <liyangyang_stick@163.com>
2019-09-16 15:06:20 +0800

    vue集成ueditor

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ueditor
-百度富文本编辑器使用
+#ueditor demo
+1.ueditor自动保存功能，去掉
+  其原理保存在本地的localstorage之中
+  1）enableAutoSave设置成false
+  2）修改ueditor.all.js，在'contentchange':function{}函数的第一行添加代码：
+     if(!me.getOpt('enableAutoSave')){return;}
+
+2.图片设置左、右、居中浮动不起作用
+  修改ueditor.config.js,在'img'后添加'script'
+
+3.表格-右键-单元格对齐方式
+  3组相同，去掉2组
+  修改ueditor.all.js,将COUNT = 9修改为COUNT = 3
+
---
+++ src/components/UEditor.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="ueditor">
+    <script :id="id" type="text/plain"></script>
+  </section>
+</template>
+
+<script>
+import "../../static/ueditor/ueditor.config.js";
+import "../../static/ueditor/ueditor.all.js";
+import "../../static/ueditor/lang/zh-cn/zh-cn.js";
+export default {
+  name: "UE",
+  props: {
+    // 自定义参数
+    config: {
+      type: Object
+    },
+    //不同编辑器必须不同的id
+    id: {
+      type: String
+    },
+    // message
+    defaultMsg: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      editor: null
+    };
+  },
+  methods: {
+    init() {
+      const _this = this;
+      this.editor = UE.getEditor(this.id, this.config); // 初始化UE
+      this.editor.addListener("ready", function() {
+        _this.editor.setContent(_this.defaultMsg); // 确保UE加载完成后，放入内容。
+      });
+    },
+    getUEContent() {
+      // 获取内容方法
+      return this.editor.getContent();
+    },
+    getUEContentTxt() {
+      // 获取纯文本内容方法
+      return this.editor.getContentTxt();
+    }
+  },
+  mounted() {
+    this.init();
+  },
+  destroyed() {
+    this.editor.destroy();
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
